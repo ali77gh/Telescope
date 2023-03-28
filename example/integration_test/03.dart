4d073a8342bd003095acc6540ae1bd10dc5e7310
Ali Ghahremani <alighahremani1377@gmail.com>
2023-03-28 12:33:59 +0330

    fix: quick fix on 03 test

--- example/integration_test/03.dart
+++ example/integration_test/03.dart
@@ -1,26 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app/01_simple_text_sample/main.dart' as example01;
+import 'package:app/03_depend_observable_sample/main.dart' as example03;
 
 void test(){
 
-  testWidgets('simple text', (tester) async {
-    example01.main();
+  testWidgets('bmi', (tester) async {
+    example03.main();
     await tester.pumpAndSettle();
+    var initResult = tester.allWidgets.whereType<Text>().last.data!;
 
-    expect(find.text(''), findsNWidgets(2));
-    expect(find.text('0'), findsOneWidget);
-
-    await tester.tap(find.text('0'));
+    await tester.tap(find.byType(Slider).first);
     await tester.pumpAndSettle(); // Trigger a frame.
 
-    expect(find.text('a'), findsNWidgets(2));
-    expect(find.text('1'), findsOneWidget);
-
-    await tester.tap(find.text('1'));
+    await tester.tap(find.byType(Slider).last);
     await tester.pumpAndSettle(); // Trigger a frame.
 
-    expect(find.text('aa'), findsNWidgets(2));
-    expect(find.text('2'), findsOneWidget);
+    var weight = tester.allWidgets.whereType<Slider>().first.value;
+    var height = tester.allWidgets.whereType<Slider>().last.value;
+    var result = tester.allWidgets.whereType<Text>().last.data!;
+
+    var bmi = weight/ ((height/ 100) * (height / 100));
+
+    expect(result.contains("$bmi"), true);
+    expect(initResult != result, true);
   });
 }
