08b34468c5e28c91fe73f800cbbca007264fd2ec
Ali Ghahremani <alighahremani1377@gmail.com>
2025-11-24 17:49:33 +0330

    ref: Telescope provider and selector removed

--- lib/telescope.dart
+++ lib/telescope.dart
@@ -4,4 +4,3 @@ export './src/telescope.dart' show Telescope;
 export './src/telescope_list.dart' show TelescopeList;
 export './src/fs/on_disk_save_ability.dart' show OnDiskSaveAbility;
 export './src/telescope_builder.dart' show TelescopeBuilder;
-export './src/telescope_provider.dart' show TelescopeProvider, TelescopeSelector;
