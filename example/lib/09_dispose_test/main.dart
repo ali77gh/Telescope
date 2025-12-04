70edcc2d69759ed91f742e49a3cf56683ad26c1b
Ali Ghahremani <alighahremani1377@gmail.com>
2025-12-04 16:31:31 +0330

    example: dispose test added
    It's working, I saw logs

---
+++ example/lib/09_dispose_test/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:telescope/telescope.dart';
+
+class DisposeTest extends StatelessWidget {
+  final visible = Telescope(false);
+  final counter = Telescope(0);
+
+  final style = const TextStyle(fontSize: 60);
+
+  DisposeTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // type: MaterialType.transparency,
+      home: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            counter.value += 1;
+            visible.value = !visible.holden;
+          },
+          child: Container(
+            color: Colors.white,
+            child: TelescopeBuilder<bool>(
+              telescope: visible,
+              builder: (context, visibility_) {
+                if (visibility_) {
+                  return TelescopeBuilder<int>(
+                    telescope: counter,
+                    builder: (context, counter_) =>
+                        Text(counter_.toString(), style: style),
+                  );
+                } else {
+                  return const Text("click to see");
+                }
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(DisposeTest());
+}
